d881ddf2d3783deb0c364ff4ae3945d077098c7a
Stefan <stefann.kostovski@gmail.com>
2026-01-01 20:28:01 +0100

    Refactored JS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import './style.css'
 
-const kamenBTN =document.getElementById("kamenBTN")         as HTMLButtonElement
-const papierBTN = document.getElementById("papierBTN")      as HTMLButtonElement
-const nozniceBTN = document.getElementById("nozniceBTN")    as HTMLButtonElement
-const novaHra = document.getElementById("novaHra")          as HTMLButtonElement
-
-const clovek =document.getElementById("clovek")             as HTMLHeadingElement
-const pocitac = document.getElementById("pocitac")          as HTMLHeadingElement
-const remiza = document.getElementById("remiza")            as HTMLHeadingElement
-const vitaz = document.getElementById("vitaz")              as HTMLHeadingElement
+const kamenBTN =    document.getElementById("kamenBTN")         as HTMLButtonElement
+const papierBTN =   document.getElementById("papierBTN")        as HTMLButtonElement
+const nozniceBTN =  document.getElementById("nozniceBTN")       as HTMLButtonElement
+const novaHra =     document.getElementById("novaHra")          as HTMLButtonElement
+const clovek =      document.getElementById("clovek")           as HTMLHeadingElement
+const pocitac =     document.getElementById("pocitac")          as HTMLHeadingElement
+const remiza =      document.getElementById("remiza")           as HTMLHeadingElement
+const vitaz =       document.getElementById("vitaz")            as HTMLHeadingElement
 
 let humanScore:number = 0
 let comptuerScore:number = 0
@@ -31,100 +30,58 @@ function checkScore () {
     clovek!.textContent = `Skore cloveka  ${humanScore} :`
     pocitac!.textContent = `${comptuerScore} Skore pocitaca`
 }
+function resetGame(){
+    novaHra!.textContent = ""
+    vitaz!.textContent = ""
 
+    humanScore = 0
+    comptuerScore = 0
+
+    checkScore()
+
+    kamenBTN.disabled = false
+    papierBTN.disabled = false
+    nozniceBTN.disabled = false
+}
 function checkWinner () {
-    if (humanScore === 5) {
-        vitaz!.textContent = "Vyhral si !!!"
+    if (humanScore === 5 || comptuerScore === 5) {
+
+        vitaz!.textContent = humanScore === 5
+                            ? "Vyhral si !!!"
+                            : "Vyhral Pocitac :{ "
+
         novaHra!.textContent = "Zacat novu hru ?"
 
         kamenBTN.disabled = true
         papierBTN.disabled = true
         nozniceBTN.disabled = true
 
-        novaHra.addEventListener("click",() => {
-            novaHra!.textContent = ""
-            vitaz!.textContent = ""
-
-            humanScore = 0
-            comptuerScore = 0
-
-            checkScore()
-
-            kamenBTN.disabled = false
-            papierBTN.disabled = false
-            nozniceBTN.disabled = false
-        })
     }
 
-    if (comptuerScore === 5) {
-        vitaz!.textContent = "Vyhral Pocitac :{"
-        novaHra!.textContent = "Zacat novu hru ?"
-
-        kamenBTN.disabled = true
-        papierBTN.disabled = true
-        nozniceBTN.disabled = true
-
-        novaHra.addEventListener("click",() => {
-            novaHra!.textContent = ""
-            vitaz!.textContent = ""
-
-            humanScore = 0
-            comptuerScore = 0
-            checkScore()
-
-            kamenBTN.disabled = false
-            papierBTN.disabled = false
-            nozniceBTN.disabled = false
-        })
-    }
 }
 
-kamenBTN?.addEventListener("click", () => {
-    const computerChoice = getComputerChoice()
+function playGame(moznostCloveka:"kamen" | "papier" | "noznice") {
 
-    if (computerChoice === "papier") {
-        comptuerScore++
-        remiza!.textContent = ""
-    } else if (computerChoice === "noznice") {
-        humanScore++
-        remiza!.textContent = ""
-    } else {
+    const computerChoice = getComputerChoice()
+    remiza!.textContent = ""
+
+    if (moznostCloveka === computerChoice){
         remiza!.textContent = "Remiza"
     }
-    checkScore()
-    checkWinner()
-})
-
-papierBTN?.addEventListener("click", () => {
-    const computerChoice = getComputerChoice()
-
-    if (computerChoice === "kamen") {
-        humanScore++
-        remiza!.textContent = ""
-    } else if (computerChoice === "noznice") {
-        comptuerScore++
-        remiza!.textContent = ""
+    else if ((moznostCloveka === "kamen" && computerChoice === "papier") ||
+            (moznostCloveka === "papier" && computerChoice === "noznice") ||
+            (moznostCloveka === "noznice" && computerChoice === "kamen")) {
+            comptuerScore++
     } else {
-        remiza!.textContent = "Remiza"
+        humanScore++
     }
 
     checkScore()
     checkWinner()
-})
-
-nozniceBTN?.addEventListener("click", () => {
-    const computerChoice = getComputerChoice()
-
-    if (computerChoice === "kamen") {
-        comptuerScore++
-        remiza!.textContent = ""
-    } else if (computerChoice === "papier") {
-        humanScore++
-        remiza!.textContent = ""
-    } else {
-        remiza!.textContent = "Remiza"
-    }
-
-    checkScore()
-    checkWinner()
+}
+kamenBTN.addEventListener   ("click", () => playGame("kamen"))
+papierBTN.addEventListener  ("click", () => playGame("papier"))
+nozniceBTN.addEventListener ("click", () => playGame("noznice"))
+novaHra.addEventListener    ("click",() => {
+    resetGame()
 })
